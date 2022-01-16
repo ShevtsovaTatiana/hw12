c86a20164c6dc54c8c3ed3c60ce25b6f7859b120
ShevtsovaTatiana <tanjaash@gmail.com>
2022-01-17 00:50:06 +0200

    initial commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,9 @@
 $(function() {
   
   $('.reviews__slider').slick({
-    prevArrow: "<button type='button' class='slick-prev slick-arrow'><svg class='reviews__slick reviews__slick--prev'><use xlink:href=images/sprite.svg#prev_arrow></use></svg></button>",
+    prevArrow: "<button type='button' class='slick-prev slick-arrow reviews__slick reviews__slick--prev'><svg class='reviews__arrow reviews__arrow--prev'><use xlink:href=images/sprite.svg#Vector></use></svg></button>",
 
-    nextArrow: "<button type='button' class='slick-next slick-arrow'><svg class='reviews__slick reviews__slick--next'><use xlink:href=images/sprite.svg#next_arrow2></use></svg></button>",
+    nextArrow: "<button type='button' class='slick-next slick-arrow reviews__slick  reviews__slick--next'><svg class='reviews__arrow reviews__arrow--next'><use xlink:href=images/sprite.svg#Vector></use></svg></button>",
 
     dots: true,
     arrows: true
